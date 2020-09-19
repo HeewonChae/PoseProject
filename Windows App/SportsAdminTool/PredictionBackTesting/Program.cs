3fac460605328ac42e9e350d37629588f0746010
HeewonChae <korman7750@hotmail.com>
2020-09-20 07:49:24 +0900

    예측 로직 변경

--- Windows App/SportsAdminTool/PredictionBackTesting/Program.cs
+++ Windows App/SportsAdminTool/PredictionBackTesting/Program.cs
@@ -25,7 +25,7 @@ namespace PredictionBackTesting
             DataMapper.Resolve<db_table.Prediction, db_table.PredictionBackTesting>();
 
             // select predicted fixtures
-            var predictedFixtures = SelecltPredictedFixtures.Execute(startDate: new DateTime(2020, 8, 15), endData: new DateTime(2020, 8, 31));
+            var predictedFixtures = SelecltPredictedFixtures.Execute(startDate: new DateTime(2020, 9, 1), endData: new DateTime(2020, 9, 30));
 
             // Predict fixture and check hit
             var dic_predictions = PredictFixtureAndCheckHit.Execute(predictedFixtures);
